1ee2759487e2614f5b60453e75b60e1ca003f63b
shipc <shipc119@163.com>
2019-06-18 19:29:41 +0800

    init-1

--- mini-eureka/src/main/java/com/mini/eureka/EurekaStart.java
+++ mini-eureka/src/main/java/com/mini/eureka/EurekaStart.java
@@ -1,13 +1,8 @@
 package com.mini.eureka;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
